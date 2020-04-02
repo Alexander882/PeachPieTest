ab2f1b032673586a14edd23bcfe0ab46f85c276f
Alexander Houmann Aalykke <alha@netcompany.com>
2020-04-02 12:00:27 +0200

    Minimal example of PHP -> PHP dependency

--- PhpTest/PhpMain/index.php
+++ PhpTest/PhpMain/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ .'/SomeInterface.php';
+
 class TroubleClass extends \DotNet\AbstractClass implements \PhpInterfaces\SomeInterface {
 	public function GetInt(): int{
 		return 1;
@@ -12,22 +14,3 @@ class TroubleClass extends \DotNet\AbstractClass implements \PhpInterfaces\SomeI
 $troubleClass = new TroubleClass();
 echo $troubleClass->GetInt()."<br>";
 echo $troubleClass->GetString()."<br>";
-
-
-
-class PhpInheritingClass extends \DotNet\DotNetBaseClass {
-	public function __construct(int $param) {
-		parent::__construct($param, "abc");
-	}
-}
-
-$implementingClass = new \DotNet\ImplementingClass();
-echo $implementingClass->GetString()."<br>";
-
-
-
-$dotNetInheritingClass = new \DotNet\DotNetInheritingClass(5);
-echo $dotNetInheritingClass->GetInt()."<br>";
-
-$phpInheritingClass = new PhpInheritingClass();
-echo $phpInheritingClass->GetInt()."<br>";
